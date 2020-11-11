9f6768f35e3b9e07c82c1e3aa09a68fa52b0e339
iuricarva <icarva@hotmail.com>
2020-11-10 22:07:14 -0300

    created basic configuration and database connection

---
+++ src/config/database.js
@@ -0,0 +1,6 @@
+const { Mongoose } = require("mongoose")
+
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://192.168.0.110/mymoney')
